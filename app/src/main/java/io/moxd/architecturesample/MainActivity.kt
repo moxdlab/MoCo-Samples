94d3a9a1e894674d722a585367ddfb9b5403e9de
David Petersen <david@familiep.de>
2022-12-06 18:56:59 +0100

    add permission to call person directly

--- app/src/main/java/io/moxd/architecturesample/MainActivity.kt
+++ app/src/main/java/io/moxd/architecturesample/MainActivity.kt
@@ -1,16 +1,21 @@
 package io.moxd.architecturesample
 
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
 import io.moxd.architecturesample.compose.MainScreen
 import io.moxd.architecturesample.ui.theme.ArchitectureSampleTheme
 import io.moxd.architecturesample.viewmodels.PersonViewModel
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,15 +26,20 @@ class MainActivity : ComponentActivity() {
             ArchitectureSampleTheme {
                 // no need for 'remember':
                 val items by viewModel.getAllPersons().observeAsState(emptyList())
+                val launcher =
+                    rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {}
 
-                MainScreen(items,
-                    onFabClicked = {
-                        viewModel.addPerson("Max Musterfrau", "+491234567890")
-                    },
+                MainScreen(
+                    items,
+                    onFabClicked = { viewModel.addPerson("Max Musterfrau", "+491234567890") },
                     onItemClicked = { person ->
-                        //discuss if this should be view, viewmodel or model related code:
-                        Intent(Intent.ACTION_DIAL, Uri.parse("tel:${person.tel}")).also {
-                            startActivity(it)
+                        when (ContextCompat.checkSelfPermission(this, CALL_PHONE)) {
+                            PackageManager.PERMISSION_GRANTED -> {
+                                Intent(Intent.ACTION_CALL, Uri.parse("tel:${person.tel}")).also {
+                                    startActivity(it)
+                                }
+                            }
+                            else -> launcher.launch(CALL_PHONE)
                         }
                     }
                 )
